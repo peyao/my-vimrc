2650722bf814054cb97dfba96a0f3fb69786e085
Peter Yao <peyao@protonmail.ch>
2023-03-29 02:44:16 -0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -192,6 +192,11 @@ require('lazy').setup({
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
+-- Set tab config
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
